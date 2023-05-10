c1a01820e6b8634acfc358998802e7ad4ebef29a
pedroJ <pedro.jofre.escarate@gmail.com>
2023-05-09 20:56:59 -0400

    termino agregar productos a capa

--- src/routes/product.router.js
+++ src/routes/product.router.js
@@ -6,69 +6,15 @@ productsRouter.use(json());
 
 productsRouter.get("/", ProductController.getProducts);
 
-/* productsRouter.post("/", async (req, res) => {
-
-    const { title, description, price, code, status, stock, category, thumbnails } = { ...req.body }
-
-    if (!title || !description || !price || !code || !stock || !category) {
-
-        return res.status(400).send({ status: "error", payload: "falta informacion title o description o price o code o category o stock)" })
-    }
-    try {
-        const newProduct = await productManager.create({ title, description, price, code, status, stock, category, thumbnails })
-        res.status(200).send({ status: "ok", payload: newProduct })
-
-    } catch (error) {
-        res.status(400).send({ status: "Error", payload: error })
-    }
-
-})
+productsRouter.post("/", ProductController.addProduct)
 
 
-productsRouter.get("/:pid", async (req, res) => {
+productsRouter.get("/:pid", ProductController.getProductById)
 
-    const { pid } = (req.params);
-    try {
+productsRouter.delete("/:pid", ProductController.deleteProduct)
 
-        const product = await productManager.getProductById(pid)
-        if (product) {
-            res.send({ status: "ok", payload: product })
-        } else {
-            res.status(400).send({ status: "error", payload: "id no encontrado" })
-        }
-    } catch (error) {
-        res.status(400).send({ stus: "eoor", payload: error })
-    }
-})
+productsRouter.put("/:pid", ProductController.updateProduct)
 
-productsRouter.delete("/:pid", async (req, res) => {
-
-    const { pid } = (req.params);
-    try {
-        const deleteProduct = await productManager.deleteProductById(pid)
-        res.send({ status: "ok", payload: deleteProduct })
-    } catch (error) {
-        res.status(400).send({ status: "error", payload: error })
-    }
-})
-
-productsRouter.put("/:pid", async (req, res) => {
-    const { pid } = (req.params);
-    const campo = { ...req.body }
-    try {
-        const updateProduct = await productManager.updateProduct(pid, campo)
-        res.status(200).send({ status: "ok", payload: updateProduct })
-    } catch (error) {
-        res.status(400).send({ status: "error", payload: error })
-    }
-})
-
-productsRouter.get("/real-time-products", async (req, res) => {
-    const products = await productManager.getAll()
-    //res.render(, { products })
-})
-
-
- */
+productsRouter.get("/real-time-products", ProductController.realTimeProducts)
 
 export default productsRouter
--- src/service/product.service.js
+++ src/service/product.service.js
@@ -7,6 +7,33 @@ class ProductService {
         const products = await productManager.getAll()
         return products
     }
+
+    static create = async (title, description, price, code, status, stock, category, thumbnails) => {
+        console.log("create")
+
+        const result = await productManager.create({ title, description, price, code, status, stock, category, thumbnails })
+
+        return result
+    }
+
+    static getById = async (id) => {
+        const result = await productManager.getProductById(id)
+        return result
+    }
+
+    static deleteById = async (id) => {
+        const result = await productManager.deleteProductById(id)
+        return result
+    }
+
+    static updateById = async (id, campo) => {
+        const result = await productManager.updateProduct(id, campo)
+    }
+
+
+
+
 }
+
 
 export { ProductService }
